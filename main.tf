552bb5449139a9bf5b2265a05dadb93356f4366d
jagadamreddykishorereddy1188 <128318993+jagadamreddykishorereddy1188@users.noreply.github.com>
2024-11-14 12:37:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "three" {
-count = 2
-ami           = "ami-0984f4b9e98be44bf"
+    count      = 2
+           ami = "ami-0984f4b9e98be44bf"
   instance_type = "t2.micro"
   tags = {
     Name = "kishore"
